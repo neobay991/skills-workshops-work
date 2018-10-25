e43538123bbe393b8c57ef751f29cddaf4fe701e
Jay Khan <jkhan992@gmail.com>
2018-10-25 13:54:30 +0100

    Example of nested objects

---
+++ practicals/nested_objects/index.js
@@ -0,0 +1,42 @@
+/*
+In our spaceship object, we want a crew object. This will contain all the crew members who do important work on the craft. Each of those crew members are objects themselves. They have properties like name, and degree, and they each have unique methods based on their roles. We can also nest other objects in the spaceship such as a telescope or nest details about the spaceship's computers inside a parent nanoelectronics object.
+*/
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032
+  },
+  crew: {
+    captain: {
+      name: 'Sandra',
+      degree: 'Computer Engineering',
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    backup: {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+};
+const capFave = spaceship.crew.captain['favorite foods'][0];
+
+/*
+Right now the passengers property has a value of null. Instead, assign as its value an array of objects. These objects should represent the spaceship's passengers as individual objects. Make at least one passenger object in the array that has at least one key-value pair on it.
+*/
+spaceship.passengers = [{name: 'jay'}, {name: 'jack'}];
+
+/*
+Create a variable firstPassenger and assign the first passenger as its value (the element in the 0th index of the spaceship.passengers array you just made). Make sure to use bracket and dot notation to get the passenger object through nested access (don't just copy the object into the variable!)
+*/
+const firstPassenger = spaceship.passengers[0];
